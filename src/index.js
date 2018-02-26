b6c41a739880f0a5b321c8a30f6260126ad192fe
Dmitry Shaido <d.shaido@gmail.com>
2018-02-26 18:21:06 +0300

    change to Math module

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = function makeExchange(currency) {
         var coins = {"H":null,"Q":null,"D":null,"N":null,"P":null};
         var denom = {"H":50,"Q":25,"D":10,"N":5,"P":1};
         for (var key in denom) {
-            coins[key] = (currency - currency % denom[key]) / denom[key];
+            coins[key] = Math.floor(currency / denom[key]);
             currency -= denom[key] * coins[key];
         }
         for (var key in coins) {
